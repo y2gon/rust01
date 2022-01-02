3ace807b379f7c1766fb3d792b367e137a0be27b
y2gon <y2gon2@naver.com>
2022-01-02 18:03:11 +0900

    1st echo server client

--- client/clientMain/src/main.rs
+++ client/clientMain/src/main.rs
@@ -1,3 +1,63 @@
+use std::net::{TcpStream};
+use std::io::{Read, Write};
+use std::str::from_utf8;
+
+const MAX_LENGTH : usize = 256; // 입력가능한 최대 문자열의 길이
+
 fn main() {
-    println!("Hello, world!");
+    // localhost:3333 에 연결이 정상적으로 될 경우와 실패할 경우를 구분
+    match TcpStream::connect("localhost:3333"){
+        // 정상적으로 연결이 된 경우,
+        Ok(mut stream) => {
+            println!("Successfully connected to server in port 3333");
+            let mut msg_length  = MAX_LENGTH + 1;
+            let mut input = String::new();
+
+            // 표준 입력 받기 (256자를 넘게 입력할 경우, 글자수 초과 메세지를 남기고 재입력 요청)
+            while msg_length > MAX_LENGTH {
+                
+                println!("Message input (Maxium length = 256): ");
+                msg_length = std::io::stdin().read_line(&mut input).unwrap();
+
+                if msg_length > MAX_LENGTH {
+                    println!("Please input message under 256 characters.");
+                    input.clear();
+                }
+            }
+
+            let mut msg = [0; MAX_LENGTH];  
+            
+            // 표준 입력 string 을 ASCII code 값의 array로 전환
+            for (idx, b_char ) in input.chars().enumerate(){
+                msg[idx] = b_char as u8;  
+            }
+            
+            // 최대 256 크기의 array에서 실제 문자값이 포함된 부분까지 slice 하여 stream 에 binary 로 전송
+            stream.write(&msg[0..(msg_length-2)]).unwrap();
+            println!("Sent Hello, awaiting reply...");
+            
+            // 서버에서 전송한 stream 을 처리
+            let mut data = [0 as u8; MAX_LENGTH]; 
+
+            // 전송된 stream 값을 data array 에 입력 하고, 해당 작업이 정상적으로 진행되었는지에 따라 처리
+            match stream.read_exact(&mut data[0..(msg_length-2)]) {
+                Ok(_) => {
+                    // client 로 재전송된 값이 기존에 server 로 보낸 message 와 동일한지 확인하여 T/F 를 분기하여 처리.
+                    if &data[0..(msg_length-2)] == &msg[0..(msg_length-2)]{
+                        println!("Receiving Message : {}" , from_utf8(&data).unwrap());
+                    } else {
+                        let text = from_utf8(&data).unwrap();
+                        println!("Unexpected reply: {}", text);
+                    }
+                },
+                Err(e) => {
+                    println!("Failed to receive data : {}", e);
+                }
+            }
+        },
+        Err(e) => {
+            println!("Failed to connect : {}", e);
+        }
+    }
+    println!("Terminated.");
 }
--- server/serverMain/src/main.rs
+++ server/serverMain/src/main.rs
@@ -1,3 +1,60 @@
+use std::thread;
+use std::net::{TcpListener, TcpStream, Shutdown};
+use std::io::{Read, Write};
+
+const MAX_LENGTH : usize = 256; // 입력가능한 최대 문자열의 길이
+
+// (server 동작 및 client 연결이 정상적일 때) 입력 값 처리 함수.
+fn handle_client(mut stream: TcpStream) {
+    let mut data = [0 as u8; MAX_LENGTH]; // 한번에 입력받을 수 있는 최대 문자열 길이를 256 자로 설정
+    
+    // client 로 부터 입력받은 stream 값을 읽어서 data 에 저장
+    while match stream.read(&mut data) {
+        
+        // data 의 크기가 정상적인 숫자의 값을 가지는 경우 (제대로 입력이 이루어졌을 때)
+        Ok(size) => {
+            // 해당 data 를 stream 에 binary  값으로 다시 전송
+            stream.write(&data[0..size]).unwrap();
+            true
+        },
+        // data 에 정상적으로 입력되지 않았을 경우, error message 를 보내고 서버를 종료.
+        Err(_) => {
+            println!("An error occurred, terminating connection with {}", stream.peer_addr().unwrap());
+            stream.shutdown(Shutdown::Both).unwrap();
+            false
+        }
+    } {}
+}
+
 fn main() {
-    println!("Hello, world!");
+    // 표준 모듈을 사용하여  Tcp 기반의 소켓을 생성하고, 페킷이 오기를 대기.
+    let listener = TcpListener::bind("127.0.0.1:3333").unwrap();
+    println!("Server listening on port 3333");
+
+    // TcpListener 가 정상적으로 작동할때의 result 를 incoming 매소드를 사용하여 반복하여 stream 변수에 할당. 
+    for stream in listener.incoming() { 
+
+        // stream 값이 정상적일 때와, 문제가 있을 때를 구분
+        match stream {
+
+            // stream 이 정상적일 때, 
+            Ok(stream) => {
+                println!("New connection: {}", stream.peer_addr().unwrap());
+                
+                // 해당 stream 을 처리할 thread 를 생성하고, 처리함수를 실행.
+                thread::spawn(move|| {
+                    // connection succeeded
+                    handle_client(stream)
+                });
+            }
+
+            // stream 에 문제가 있을 때
+            Err(e) => {
+                println!("Error: {}", e);
+                /* connection failed */
+            }
+        }
+    }
+    // close the socket server
+    drop(listener);
 }
