916dff3dc52e2f4530169e5c99e4643f6138c7dd
y2gon <y2gon2@naver.com>
2022-01-07 21:12:20 +0900

    3rd Assignment --ing

--- client/clientMain/src/main.rs
+++ client/clientMain/src/main.rs
@@ -1,8 +1,11 @@
-use std::net::{TcpStream};
+use native_tls::TlsConnector;
+use std::net::TcpStream;
 use std::io::{Read, Write};
 use std::str::from_utf8;
+use std::fs::File;
 
-const MAX_LENGTH : usize = 256; // 입력가능한 최대 문자열의 길이
+const CODE_LENGTH : usize = 3; // 1 글자만 입력 허용 (2 는 개행시 입력되는 코드를 받기 위한 buffer)
+const CONTENTS_LENGTH : usize = 256; // 파일 내용의 글자수를 전송할 수있는 최대 크기를 256 자 이내로 제한.
 
 fn main() {
     // localhost:3333 에 연결이 정상적으로 될 경우와 실패할 경우를 구분
@@ -10,51 +13,67 @@ fn main() {
         // 정상적으로 연결이 된 경우,
         Ok(mut stream) => {
             println!("Successfully connected to server in port 3333");
-            let mut msg_length  = MAX_LENGTH + 1;
+            let mut msg_length  = CODE_LENGTH + 1;
             let mut input = String::new();
 
-            // 표준 입력 받기 (256자를 넘게 입력할 경우, 글자수 초과 메세지를 남기고 재입력 요청)
-            while msg_length > MAX_LENGTH {
+            // 표준 입력 받기 (1자를 넘게 입력할 경우, 글자수 초과 메세지를 남기고 재입력 요청)
+            while msg_length > CODE_LENGTH {
                 
-                println!("Message input (Maxium length = 256): ");
+                println!("Please input the access code (Maxium length : {}): ", CODE_LENGTH-2);
                 msg_length = std::io::stdin().read_line(&mut input).unwrap();
 
-                if msg_length > MAX_LENGTH {
-                    println!("Please input message under 256 characters.");
+                if msg_length > CODE_LENGTH {
+                    println!("Please input one chararter.");
                     input.clear();
                 }
             }
-
-            let mut msg = [0; MAX_LENGTH];  
             
             // 표준 입력 string 을 ASCII code 값의 array로 전환
-            for (idx, b_char ) in input.chars().enumerate(){
-                msg[idx] = b_char as u8;  
-            }
+            let msg = input.as_bytes();
             
-            // 최대 256 크기의 array에서 실제 문자값이 포함된 부분까지 slice 하여 stream 에 binary 로 전송
-            stream.write(&msg[0..(msg_length-2)]).unwrap();
-            println!("Sent Hello, awaiting reply...");
-            
-            // 서버에서 전송한 stream 을 처리
-            let mut data = [0 as u8; MAX_LENGTH]; 
+            // binary 로 변환된 입력값을 전송
+            stream.write(&msg).unwrap();
+            println!("Sent the code, awaiting reply...");
 
-            // 전송된 stream 값을 data array 에 입력 하고, 해당 작업이 정상적으로 진행되었는지에 따라 처리
-            match stream.read_exact(&mut data[0..(msg_length-2)]) {
+            // Server로부터 전송된 stream 값을 data array 에 입력
+            let mut fromServer:[u8;1] =[0];
+            match stream.read_exact(&mut fromServer) {
                 Ok(_) => {
-                    // client 로 재전송된 값이 기존에 server 로 보낸 message 와 동일한지 확인하여 T/F 를 분기하여 처리.
-                    if &data[0..(msg_length-2)] == &msg[0..(msg_length-2)]{
-                        println!("Receiving Message : {}" , from_utf8(&data).unwrap());
+                    // client 로 재전송된 값이 기존에 server 로 보낸 message 와 동일한지 확인하여 T("1")/F(_) 를 분기하여 처리.
+                    if from_utf8(&mut fromServer).unwrap() == "1" {
+                        //FTP protocol 을 사용한 통신 진행
+                        let connector = TlsConnector::new().unwrap();
+                        let stream = TcpStream::connect("127.0.0.1:8443").unwrap();
+                        let mut stream = connector.connect("127.0.0.1:8443", stream).unwrap();
+
+                        println!("Accepted! start loading a file. Please input the file name. (.txt file only)");
+
+                        // 파일명을 표준입럭 받아서 server 에 전송
+                        std::io::stdin().read_line(&mut input).unwrap();
+
+                        match File::open(&mut input) {
+                            // 해당 파일이 존재 할때 
+                            Ok(mut file) => { 
+                                let mut data :[u8;CONTENTS_LENGTH] = [0 as u8; 256];
+                                file.read(&mut data).unwrap();
+
+                                let mut sendingData = data.as_bytes();
+                                stream.write(&sendingData).unwrap();
+                                println!("Transmission complete!");
+                            }
+                            Err(_) => {
+                                println!("The file is not exist.")
+                            }
+                        }
                     } else {
-                        let text = from_utf8(&data).unwrap();
-                        println!("Unexpected reply: {}", text);
+                        println!("Denied to upload a file");
                     }
-                },
+                }
                 Err(e) => {
                     println!("Failed to receive data : {}", e);
                 }
             }
-        },
+        }
         Err(e) => {
             println!("Failed to connect : {}", e);
         }
--- server/serverMain/src/main.rs
+++ server/serverMain/src/main.rs
@@ -1,21 +1,64 @@
 use std::thread;
 use std::net::{TcpListener, TcpStream, Shutdown};
 use std::io::{Read, Write};
+use std::str::from_utf8;
+use native_tls::{Identity, TlsAcceptor, TlsStream};
+use std::sync::Arc;
+use std::fs::File;
 
-const MAX_LENGTH : usize = 256; // 입력가능한 최대 문자열의 길이
-
+const CODE_LENGTH : usize = 3;   // FTP 통신을 위한 입력값에 대해 입력가능한 최대 문자열의 길이
+const FILE_DATA_LENGTH : usize = 256; // 전송된 파일에 대한 Data 에 대해 입력 가능한 최대 문자열의 길이
+const CODE : &str = "1";        // FTP 접속 승인 code
+ 
 // (server 동작 및 client 연결이 정상적일 때) 입력 값 처리 함수.
 fn handle_client(mut stream: TcpStream) {
-    let mut data = [0 as u8; MAX_LENGTH]; // 한번에 입력받을 수 있는 최대 문자열 길이를 256 자로 설정
-    
+    let mut data = [0 as u8; CODE_LENGTH];  // 전송된 CODE 값을 입력받을 array (CODE : 1 , 개행문자 : 2)
+    let mut status =[0 as u8; 1];           // client 에 T/F 현황을 전송할 array ("1" == True / "0" == False)
     // client 로 부터 입력받은 stream 값을 읽어서 data 에 저장
     while match stream.read(&mut data) {
-        
         // data 의 크기가 정상적인 숫자의 값을 가지는 경우 (제대로 입력이 이루어졌을 때)
-        Ok(size) => {
-            // 해당 data 를 stream 에 binary  값으로 다시 전송
-            stream.write(&data[0..size]).unwrap();
-            true
+        Ok(_size) => {
+            if from_utf8(&mut data[0..(CODE_LENGTH-2)]).unwrap() == CODE{
+                status[0] = '1' as u8;              // '1' (accepted) 를 cleint 로 보냄
+                stream.write(&status).unwrap();
+
+                // FTP 통신을 위한 준비 및 thread 생성
+                let identity = Identity::from_pkcs12(&mut data[0..(CODE_LENGTH-2)], CODE).unwrap();
+                let listener = TcpListener::bind("127.0.0.1:8443").unwrap();
+                let acceptor = TlsAcceptor::new(identity).unwrap();
+                let acceptor = Arc::new(acceptor);
+                
+                for fileStream in listener.incoming() {
+                    match fileStream{
+                        Ok(fileStream) => {
+                            let acceptor = acceptor.clone();
+
+                            // FTP Data 통신에 사용할 Thread 생성
+                            thread::spawn(move||{
+                                let fileStream = acceptor.accept(fileStream).unwrap();
+                                
+                                let mut fileData = [0 as u8; FILE_DATA_LENGTH];
+                                while match fileStream.read(&mut fileData){
+                                    Ok(size) => {
+                                        let title:&str = fileData[0..size].chars();
+                                        let mut file = File::create(title);
+
+                                    }
+                                    Err(e) => {println!("Error: {}", e)}
+                                
+                            });
+                            
+                        }
+                        Err(e) => {println!("Error: {}", e)}
+                    }
+                }
+                true
+            } else {
+                status[0] = '0' as u8;              // client 입력 코드가 server 에서 가지고 있는 code 값과 다를 경우 "0" (False) 를 client 로 전송
+                stream.write(&status).unwrap();
+                false
+            }
+            
         },
         // data 에 정상적으로 입력되지 않았을 경우, error message 를 보내고 서버를 종료.
         Err(_) => {
@@ -30,6 +73,7 @@ fn main() {
     // 표준 모듈을 사용하여  Tcp 기반의 소켓을 생성하고, 페킷이 오기를 대기.
     let listener = TcpListener::bind("127.0.0.1:3333").unwrap();
     println!("Server listening on port 3333");
+    let mut acceptor:bool = false;
 
     // TcpListener 가 정상적으로 작동할때의 result 를 incoming 매소드를 사용하여 반복하여 stream 변수에 할당. 
     for stream in listener.incoming() { 
@@ -44,15 +88,16 @@ fn main() {
                 // 해당 stream 을 처리할 thread 를 생성하고, 처리함수를 실행.
                 thread::spawn(move|| {
                     // connection succeeded
-                    handle_client(stream)
+                handle_client(stream);
                 });
             }
-
             // stream 에 문제가 있을 때
             Err(e) => {
                 println!("Error: {}", e);
                 /* connection failed */
             }
+
+
         }
     }
     // close the socket server
